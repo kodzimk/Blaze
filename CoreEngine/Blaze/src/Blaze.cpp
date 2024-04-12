c2f1ba5f834ce6dff656f13d7a1c70756242635b
kodzimk <proggamedev@gmail.com>
2024-04-12 22:14:36 +0500

    Add project files.

---
+++ CoreEngine/Blaze/src/Blaze.cpp
@@ -0,0 +1,11 @@
+#include"Blaze.h"
+#include<iostream>
+#include<GL/glew.h>
+
+
+namespace Blaze {
+	void print()
+	{
+		std::cout << "Core engine activated";
+	}
+}
---
+++ CoreEngine/Blaze/src/Blaze.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Blaze {
+	void print();
+}
